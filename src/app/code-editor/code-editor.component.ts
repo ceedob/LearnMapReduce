63c1acc64d5239e2996cf838e5296e2bc6cc30b0
Chris Dobson <chris@penguinleaf.com>
2017-03-23 16:20:14 -0400

    Added default code

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-code-editor',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./code-editor.component.css']
 })
 export class CodeEditorComponent implements OnInit {
-
+  @Input() title: string;
+  code: string;
+  fn_header: string;
   constructor() { }
 
   ngOnInit() {
+    if(this.title == "Map"){
+      this.fn_header = "input_key, input_value";
+      this.code = `words = input_value.split();
+for(var w = 0; w < words.length; w++){
+    emit_intermediate(words[w], 1);
+}`;
+    }
+    else if(this.title == "Reduce"){
+      this.fn_header = "intermediate_key, intermediate_values";
+      this.code = `result = 0;
+for(var i = 0; i < intermediate_values.length; i++){
+    result += intermediate_values[i];
+}
+emit(intermediate_key, result);`;
+    }
   }
 
 }
